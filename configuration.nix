b8121cfaa4c2df1597e9f713cc43ed8eab9f14d2
satsaholic <satsaholic@earth>
2020-08-30 03:11:36 +0000

    enable dns server and serve up a zone file

--- configuration.nix
+++ configuration.nix
@@ -16,4 +16,16 @@
   users.users.root.openssh.authorizedKeys.keys = [
      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIT7z0zlSGVnGsHKLEcGfgRz0VyPRX4X9UOuorPVdgfR"
   ];
+
+  services.bind = {
+    enable = true;
+    zones = [
+      { name = "satsaholic.duckdns.org"; 
+        master = true; 
+        file = "satsaholic.duckdns.org.zone"; 
+        masters = []; slaves = [];
+        extraConfig = "";
+      }
+    ];
+  };
 }
