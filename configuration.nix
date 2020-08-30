c0e32fc6c5fbe142191a8d4edd14d016e2b17203
satsaholic <satsaholic@earth>
2020-08-30 04:35:33 +0000

    first commit after running nixos-infect script on a https://bitclouds.sh/create/ubuntu instance

---
+++ configuration.nix
@@ -0,0 +1,19 @@
+{ pkgs, ... }: {
+  environment.systemPackages = with pkgs; [
+    git
+  ];  
+
+  imports = [
+    ./hardware-configuration.nix
+    
+    
+  ];
+
+  boot.cleanTmpDir = true;
+  networking.hostName = "larawag-419";
+  networking.firewall.allowPing = true;
+  services.openssh.enable = true;
+  users.users.root.openssh.authorizedKeys.keys = [
+     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIT7z0zlSGVnGsHKLEcGfgRz0VyPRX4X9UOuorPVdgfR"
+  ];
+}
